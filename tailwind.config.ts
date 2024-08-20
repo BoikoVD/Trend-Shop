7711dfb2679ae89475926cbc3f47f396a138b754
BoikoVD <boiko.volodymyr.dm@gmail.com>
2024-08-21 01:22:09 +0200

    chore: setup base tsailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,12 +2,32 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/features/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        inter: ["var(--inter)"]
+      },
+      colors: {
+        white: "#FFFFFF",
+        black: "#000000"
+      }
+    },
+    screens: {
+      xs: "340px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+      "3xl": "1920px",
+      "4xl": "2560px",
+      "5xl": "3840px",
+      "6xl": "7680px"
+    }
   },
   plugins: []
 };
