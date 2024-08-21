226f34c08e7e09a95bc8a7e774382221b7c4021b
BoikoVD <boiko.volodymyr.dm@gmail.com>
2024-08-21 04:16:04 +0200

    style: add color palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,19 +9,31 @@ const config: Config = {
   theme: {
     extend: {
       fontFamily: {
-        inter: ["var(--inter)"]
+        "inter": ["var(--inter)"]
       },
       colors: {
-        white: "#FFFFFF",
-        black: "#000000"
+        "white": "#FFFFFF",
+        "black": "#000000",
+        'purple-100':'#A685FA',
+        'purple-300':'#8254F8',
+        'purple-400':'#703BF7',
+        'gray-50':'#999999',
+        'gray-100':'#4D4D4D',
+        'gray-200':'#333333',
+        'gray-300':'#262626',
+        'gray-400':'#1A1A1A',
+        'gray-500':'#141414',
+        "light-100":'#FCFCFC',
+        "light-200":'#F7F7F7',
+        "light-300":'#F3F3F3',
       }
     },
     screens: {
-      xs: "340px",
-      sm: "640px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1280px",
+      "xs": "340px",
+      "sm": "640px",
+      "md": "768px",
+      "lg": "1024px",
+      "xl": "1280px",
       "2xl": "1536px",
       "3xl": "1920px",
       "4xl": "2560px",
