0f599472457053f7c9c5e37e16e118691bec813e
BoikoVD <boiko.volodymyr.dm@gmail.com>
2024-08-26 01:00:13 +0200

    fix: add redurect to login page in add to cart button

--- src/features/products/components/AddToCartButton/index.tsx
+++ src/features/products/components/AddToCartButton/index.tsx
@@ -1,5 +1,8 @@
 "use client";
 import Button from "@/components/UI/Button/Button";
+import { routes } from "@/constants/routes";
+import { useRouter } from "next/navigation";
+import { useUser } from "@/features/auth";
 import { useCart } from "../../contexts/CartContext";
 import { Product } from "../../schemas/schemas";
 
@@ -15,13 +18,19 @@ export function AddToCartButton({
   className
 }: IProps) {
   const { addProductToCart } = useCart();
+  const { user } = useUser();
+  const router = useRouter();
+
+  const addToCartHandler = () => {
+    if (user) {
+      addProductToCart(product);
+    } else {
+      router.push(routes.LOGIN);
+    }
+  };
 
   return (
-    <Button
-      onClick={() => addProductToCart(product)}
-      className={className}
-      size={size}
-    >
+    <Button onClick={addToCartHandler} className={className} size={size}>
       Add To Cart
     </Button>
   );
