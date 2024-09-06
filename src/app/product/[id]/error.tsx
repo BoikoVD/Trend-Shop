fd6218f59a8ee5a5e047331b4c36d98ea7435660
BoikoVD <boiko.volodymyr.dm@gmail.com>
2024-09-06 18:09:38 +0200

    feat: add error component to product page

---
+++ src/app/product/[id]/error.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { routes } from "@/constants/routes";
+import Button from "@/components/UI/Button/Button";
+
+export default function Error({
+  error,
+  reset
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  const router = useRouter();
+
+  useEffect(() => {
+    console.log("Product page ERROR: ", error);
+  }, [error]);
+
+  return (
+    <div className="container flex min-h-screen flex-col items-center justify-center gap-6">
+      <h2 className="text-2xl">Something went wrong!</h2>
+      <div className="flex gap-4">
+        <Button onClick={() => router.push(routes.HOME)} color="dark">
+          Go Home
+        </Button>
+        <Button onClick={() => reset()}>Try Again</Button>
+      </div>
+    </div>
+  );
+}
